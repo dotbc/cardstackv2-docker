dff6dee69971cacc5ac882a61921eb691e2445b9
Alisson <thelinuxlich@gmail.com>
2020-11-20 12:53:28 -0300

    separate realms in a folder

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN yarn global add lerna
 RUN lerna bootstrap
 RUN yarn compile
 WORKDIR /cardstack/packages/cardhost
+COPY ./realms/bunny.json /cardstack/packages/cardhost/bunny.json
 COPY init.sh /cardstack/packages/cardhost/init.sh
 RUN chmod +x /cardstack/packages/cardhost/init.sh
 RUN npm install
--- init.sh
+++ init.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
-FILE=/cardstack/packages/cardhost/initialized
-if test -f "$FILE"; then
-    yarn start
-    echo ""
-    echo "Already initialized, no need to bootstrap remote realm."
-else
-    echo ""
-    echo "Bootstrapping remote realm..."
-    rm -rf /.cardstack
-    yarn start-hub &
-    sleep 10
-    echo ""
-    echo "Sending Bunny Records realm data..."
-    curl -d '{
-    "data": {
-        "attributes": {
-        "csDescription": "This is the Bunny Records realm.",
-        "csId": "https://builder-hub.stack.cards/api/realms/bunny-records",
-        "csTitle": "Bunny Records Realm",
-        "remoteCacheDir": "bunny-records-realm",
-        "remoteUrl": "https://github.com/dotbc/fuga-v2-cards.git"
-        },
-        "relationships": {
-        "csAdoptsFrom": {
-            "data": {
-            "id": "https://base.cardstack.com/public/cards/git-realm",
-            "type": "cards"
-            }
-        }
-        },
-        "type": "cards"
-    }
-    }' -H "Content-Type: application/vnd.api+json" -X POST http://localhost:3000/api/realms/meta/cards
-    echo ""
-    echo "Waiting on hub realm sync, this might take a minute..."
-    sleep 10
-    echo ""
-    echo "Killing the hub process..."
-    pkill -f node
-    sleep 30
-    echo ""
-    echo "Restarting whole hub for reindexing..."
-    yarn start
-    touch initialized
-fi
+echo ""
+echo "Bootstrapping remote realm..."
+rm -rf /.cardstack
+yarn start-hub &
+sleep 10
+echo ""
+echo "Sending Bunny Records realm data..."
+curl -d '@/cardstack/packages/cardhost/bunny.json' -H "Content-Type: application/vnd.api+json" -X POST http://localhost:3000/api/realms/meta/cards
+echo ""
+echo "Waiting on hub realm sync, this might take a minute..."
+pkill -f node
+sleep 30
+echo ""
+echo "Restarting whole hub for reindexing..."
+yarn start
+touch initialized
